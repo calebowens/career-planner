a3652cc97e0f0df3112817aa4adcd3291dff9801
Caleb Owens <hello@calebowens.com>
2024-02-12 01:48:47 +0000

    Remove broken JS

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import Alpine from "alpinejs"
-import * as Routes from "routes"
 
 Alpine.data("notificationController", (initialNotices = []) => ({
     notices: [],
@@ -38,7 +37,6 @@ Alpine.data("notificationController", (initialNotices = []) => ({
 }))
 
 
-window.Routes = Routes
 window.Alpine = Alpine
 
 Alpine.start()
